304b9ce9da99104c6bd2696db8f5c2b885ff7cd7
Serski <159945982+Serski@users.noreply.github.com>
2025-08-10 22:47:58 +0300

    Register all commands in deploy script

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
-const { REST, Routes, SlashCommandBuilder } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+const { REST, Routes } = require('discord.js');
 
-const commands = [
-  new SlashCommandBuilder()
-    .setName('postad')
-    .setDescription('Post a random ad to the news-feed (admin only)')
-    .toJSON()
-];
+const commands = [];
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(path.join(commandsPath, file));
+  commands.push(command.data.toJSON());
+}
 
 const rest = new REST({ version: '10' }).setToken(process.env.DISCORD_TOKEN);
 
