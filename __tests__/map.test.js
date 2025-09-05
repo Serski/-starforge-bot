151f5c200d36c6e01f9ad18c0dcf63216d85191d
Serski <159945982+Serski@users.noreply.github.com>
2025-09-05 18:37:50 +0300

    feat: update Ashen Verge sector map image

--- __tests__/map.test.js
+++ __tests__/map.test.js
@@ -40,12 +40,42 @@ describe('map command', () => {
     await collect({ customId: 'sector_select', values: ['nullwek'], update });
     args = update.mock.calls[0][0];
     expect(args.embeds[0].data.title).toBe('THE NULLWEK SECTOR');
+    expect(args.embeds[0].data.image.url).toBe('https://i.imgur.com/OvKSoNl.jpeg');
     expect(args.components[0].components[0].data.custom_id).toBe('nullwek_select');
 
     update.mockReset();
     await collect({ customId: 'nullwek_select', values: ['thalyron'], update });
     args = update.mock.calls[0][0];
     expect(args.embeds[0].data.title).toBe('Thalyron');
+    expect(args.components[0].components[0].data.custom_id).toBe('back_sector');
+
+    update.mockReset();
+    await collect({ customId: 'back_sector', update });
+    args = update.mock.calls[0][0];
+    expect(args.components[0].components[0].data.custom_id).toBe('sector_select');
+  });
+
+  test('sector and system flow for Ashen Verge', async () => {
+    const { handlers } = await setup();
+    const collect = handlers.collect;
+
+    const update = jest.fn().mockResolvedValue();
+
+    await collect({ customId: 'cat_sectors', update });
+    let args = update.mock.calls[0][0];
+    expect(args.components[0].components[0].data.custom_id).toBe('sector_select');
+
+    update.mockReset();
+    await collect({ customId: 'sector_select', values: ['ashen'], update });
+    args = update.mock.calls[0][0];
+    expect(args.embeds[0].data.title).toBe('ASHEN VERGE SECTOR');
+    expect(args.embeds[0].data.image.url).toBe('https://i.imgur.com/hyMsa2M.jpeg');
+    expect(args.components[0].components[0].data.custom_id).toBe('ashen_select');
+
+    update.mockReset();
+    await collect({ customId: 'ashen_select', values: ['serothis'], update });
+    args = update.mock.calls[0][0];
+    expect(args.embeds[0].data.title).toBe('Serothis');
     expect(args.components[0].components[0].data.custom_id).toBe('back_sector');
 
     update.mockReset();
